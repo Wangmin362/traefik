1c9cc64f74405a408e74cc96c2c164b48ff98fde
wangmin <wangmin@skyguard.com.cn>
2024-04-27 11:48:28 +0800

    feat: 添加注释

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -7,6 +7,10 @@ import (
 
 // Provider defines methods of a provider.
 // TODO 如何理解Traefik Provider的抽象？
+// 1、Provider其实就是向Traefik提供动态配置的组件。
+// 2、在不同的场景中，不同的Provider只需要根据特定的场景监听感兴趣的数据，与此同时生成合适的动态配置，通过Provide接口把配置交给Traefik结论。
+// 对于Docker来说，只需要监听Docker Event,看看当前的Event所对应的容器是否是用户关心的容器，如果是那么就生成合适的配置。对于K8S平台来说，
+// Provider需要通过WATCH API，监听Deployment或者Pod的变化，然后生成合适的配置即可。
 type Provider interface {
 	// Provide allows the provider to provide configurations to traefik
 	// using the given configuration channel.
