25f3915da1479fac08f24d333bedc80e1198a431
wangmin <wangmin@skyguard.com.cn>
2024-05-23 15:10:12 +0800

    feat: socks5请求代理

--- pkg/server/service/socks5.go
+++ pkg/server/service/socks5.go
@@ -1,10 +1,13 @@
 package service
 
 import (
-	"golang.org/x/net/http/httpproxy"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
+
+	"github.com/traefik/traefik/v2/pkg/log"
+	"golang.org/x/net/http/httpproxy"
 )
 
 type Socks5Handler struct {
@@ -18,7 +21,8 @@ func NewSocks5Handler(proxy *httputil.ReverseProxy) http.Handler {
 
 func (f *Socks5Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	host := req.URL.Host
-	if host == "socks5://172.30.3.224:10805" {
+	scheme := req.URL.Scheme
+	if host == "172.30.3.224:10805" && scheme == "socks5" {
 		// 设置SOCKS代理
 		tp := f.proxy.Transport.(*KerberosRoundTripper)
 		smtp := tp.OriginalRoundTripper.(*smartRoundTripper)
@@ -33,7 +37,57 @@ func (f *Socks5Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 			return hpc.ProxyFunc()(r.URL)
 		}
 
-		req.URL.Scheme = "http"
-		req.URL.Host = "172.30.3.236:8090"
+		url, _ := url.Parse("http://172.30.3.236:8090")
+		rewriteRequestURL(req, url)
 	}
+
+	f.proxy.ServeHTTP(rw, req)
+	if req.RequestURI == "/rest/wrm/2.0/resources" {
+		log.FromContext(req.Context()).Debugf("%v", req.RequestURI)
+	}
+}
+
+func rewriteRequestURL(req *http.Request, target *url.URL) {
+	targetQuery := target.RawQuery
+	req.URL.Scheme = target.Scheme
+	req.URL.Host = target.Host
+	req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+	if targetQuery == "" || req.URL.RawQuery == "" {
+		req.URL.RawQuery = targetQuery + req.URL.RawQuery
+	} else {
+		req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+	}
+}
+
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
